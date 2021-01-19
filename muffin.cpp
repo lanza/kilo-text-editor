d019848298098f7d8572d7cc7f23355b37a9cf30
Nathan Lanza <nathan@lanza.io>
2021-01-19 03:28:43 -0500

    sample files

---
+++ muffin.cpp
@@ -0,0 +1,5 @@
+int main() {
+  // do the right thing muffin
+  int a = 4;
+  return a;
+}
--- test.c
+++ test.c
@@ -1,12 +1,14 @@
+
 // muffin was here
 int main(int argc, char const** argv);
+
 /* muffin
 what are you doing
 why are you here I thought you moved
 */
 
 char const* paragraph = "muffin was here";
-
+char const* muffin = "She was a dog.";
 int main(int argc, char const** argv) {
   printf("%d\n", 4);
   for (int i = 0; i < 44; ++i) {
